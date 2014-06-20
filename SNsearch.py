8350a65167e62236eaaf1bed5f34ca2f09bc33bd
nkazmi2 <nkazmi2@illinois.edu>
2014-06-20 12:11:19 -0500

    messing with fits
    need a more clever way to get the images I want

--- SNsearch.py
+++ SNsearch.py
@@ -7,16 +7,15 @@ Created on Mon Jun 16 10:34:33 2014
 
 from astropy.io import fits
 import numpy as np
+#from numpy.linalg import inv
+import Image as pil
 import os
 
 ######################## Functions ##############################
 
 def savefig(f,quadrant):
     for k in range(len(quadrant)):
-        if (os.path.isfile(f[:-13] + str(k+1) + ".fits") == True ):
-            os.remove(f[:-13] + str(k+1) + ".fits")
-        elif (os.path.isfile(f[:-13] + str(k+1) + ".fits") == False ): 
-            fits.writeto(f[:-13] + str(k+1) + ".fits", quadrant[k])
+        fits.writeto(f[:-13] + "quad_" + str(k+1) + ".fits", quadrant[k] , clobber=True)
 
 ###################### Variable ################################
 
@@ -32,84 +31,53 @@ for m in range(len(title)):
     cx = []
     quadrant = []
     
-    image = fits.open(title[m],mode='copyonwrite', memmap=True)
+    image = fits.open(title[m], mode='copyonwrite', memmap=True)
     
-    name = image[0].header['targname']
-    header = image.info() 
+    name    = image[0].header['targname']
+    header  = image.info() 
     scidata = image[0].data 
+    #scidata = np.asmatrix(scidata)
 
     ############### Crop and subtract ###############
-    for n in range(150):
-        for p in range(200):   
+    x1 = 150
+    y1 = 200
+    x2 = 110
+    y2 = 175
+    for n in range(x1):
+        for p in range(y1):   
              bx.append(scidata[3370-n][3400+p])
              dx.append(scidata[3370+n][3400-p])
-    for i in range(110):
-        for j in range(175):
+    for i in range(x2):
+        for j in range(y2):
             ax.append(scidata[3370+i][3400+j])
             cx.append(scidata[3370-i][3400-j])  
 
-    b = np.reshape( bx , (150,200) )
-    d = np.reshape( dx , (150,200) )          
-    a = np.reshape( ax , (110,175) )
-    c = np.reshape( cx , (110,175) )
+    b = np.reshape( bx , (x1,y1) )
+    d = np.reshape( dx , (x1,y1) )          
+    a = np.reshape( ax , (x2,y2) )
+    c = np.reshape( cx , (x2,y2) )
 
-    quad2 = b - d
-    quad4 = d - b
+    quad2 = np.matrix( b - d )
+    #quad2 = np.swapaxes(quad2,0,1)
+    quad2 = np.fliplr(quad2)
     
-    quad1 = a - c
-    quad3 = c - a
+    quad4 = np.matrix( d - b )
+    quad4 = np.fliplr(quad4)
+    #quad4 = np.flipud(quad4)
     
+    quad1 = np.matrix( a - c )
+    
+    quad3 = np.matrix( c - a ) 
+    #quad3 = np.swapaxes(quad3,0,1)
+    quad3 = np.fliplr(np.flipud(quad3))
+ 
     ############### Save the images ################
     quadrant = [quad1,quad2,quad3,quad4]
     savefig(title[m],quadrant)
     
 
+###################### Coordinates ############################  
 """
-print image.info()
-header = image.info() # I think this is the header
-scidata = image[0].data # scidata is the image, pixel is a location
-print np.shape(scidata)
-cent = scidata[3356][3381] # what I chose to be center
-
-for i in range(150):
-    for j in range(200):
-            bx.append(scidata[3370-i][3400+j])
-            dx.append(scidata[3370+i][3400-j])
-for i in range(110):
-    for j in range(175):
-            ax.append(scidata[3370+i][3400+j])
-            cx.append(scidata[3370-i][3400-j])           
-            
-b = np.reshape(bx, (150,200))
-d = np.reshape(dx, (150,200))
-a = np.reshape(ax, (110,175))
-c = np.reshape(cx, (110,175))
-
-quad2 = b - d
-quad4 = d - b
-quad1 = a - c
-quad3 = c - a
-
-quadrant = [quad1,quad2,quad3,quad4]
-savefig(f,quadrant)
-
-if (os.path.isfile(f[:-13] + "1.fits") == True ):
-    os.remove(f[:-13] + "1.fits")
-elif (os.path.isfile(f[:-13] + "1.fits") == False ): 
-    fits.writeto(f[:-13] + "1.fits", quad1)
-    
-if (os.path.isfile(f[:-13] + "2.fits") == True ):
-    os.remove(f[:-13] + "2.fits")
-elif (os.path.isfile(f[:-13] + "1.fits") == False ): 
-    fits.writeto(f[:-13] + "2.fits", quad2)
-    
-if (os.path.isfile(f[:-13] + "3.fits") == True ):
-    os.remove(f[:-13] + "3.fits")
-elif (os.path.isfile(f[:-13] + "1.fits") == False ):    
-    fits.writeto(f[:-13] + "3.fits", quad3)
-    
-if (os.path.isfile(f[:-13] + "4.fits") == True ):
-    os.remove(f[:-13] + "4.fits")
-elif (os.path.isfile(f[:-13] + "1.fits") == False ): 
-    fits.writeto(f[:-13] + "4.fits", quad4)
+take the original coordinates and convert them to the new fits file coord
+rotate the fits files
 """
